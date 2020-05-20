ef8bb853c6a41601d88f5496d1511dd303586773
Andres D' Amelio <andresdamelio24@gmail.com>
2020-05-20 18:51:31 -0400

    Trabajando con archivo json estatico, uso de future builder

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,6 @@
+
 import 'package:flutter/material.dart';
+import 'package:componentes_flutter/src/providers/menu_provider.dart';
 
 
 class HomePage extends StatelessWidget {
@@ -14,18 +16,36 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _list() {
-    return ListView(
-      children: _listItems(),
+
+    // menuProvider.loadData();
+    return FutureBuilder(
+      future: menuProvider.loadData(),
+      initialData: [],
+      builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+        return ListView(
+          children: _listItems(snapshot.data),
+        );
+      }
     );
   }
 
-  List<Widget>_listItems() {
-    return [
-      ListTile(title: Text('Hola Mundo!')),
-      Divider(),
-      ListTile(title: Text('Hola Mundo!')),
-      Divider(),
-      ListTile(title: Text('Hola Mundo!'))
-    ];
+  List<Widget>_listItems(List<dynamic> data) {
+    final List<Widget> options = [];
+
+    data.forEach( (option) {
+      final widgetTemp = ListTile(
+        leading: Icon( Icons.add, color: Colors.blue, ),
+        title: Text(option['texto']),
+        trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
+        onTap: () {
+
+        },
+      );
+
+      options..add(widgetTemp)
+            ..add(Divider());
+    });
+
+    return options;
   }
 }
