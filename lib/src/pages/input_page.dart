97cf7e23c4bfd4329bb3ac5a2ec1b3282fbd6b1d
Andres D' Amelio <andresdamelio24@gmail.com>
2020-05-22 23:55:47 -0400

    Widget DropDown

--- lib/src/pages/input_page.dart
+++ lib/src/pages/input_page.dart
@@ -10,6 +10,14 @@ class _InputPageState extends State<InputPage> {
   String _name = 'Jhon Doe';
   String _email = 'example@gmail.com';
   String _fecha = '';
+  String _optionSelected = 'Volar';
+
+  List<String> _powers = [
+    'Volar',
+    'Rayos X',
+    'Super Aliento',
+    'Super Fuerza'
+  ];
 
   TextEditingController _inputFieldDateController = new TextEditingController();
 
@@ -30,12 +38,16 @@ class _InputPageState extends State<InputPage> {
           Divider(),
           _createDate( context ),
           Divider(),
+          _createDropDown(),
+          Divider(),
           _createPerson()
         ]
       )
     );
   }
 
+
+  /* Input de tipo texto */
   Widget _createInput() {
     return TextField(
       autofocus: false,
@@ -106,13 +118,6 @@ class _InputPageState extends State<InputPage> {
     );
   }
 
-  Widget _createPerson() {
-    return ListTile(
-      title: Text('Nombre: $_name '),
-      subtitle: Text('Email: $_email '),
-    );
-  }
-
   void _showSelect(BuildContext context) async {
     DateTime picked = await showDatePicker(
       context: context,
@@ -128,6 +133,49 @@ class _InputPageState extends State<InputPage> {
         _inputFieldDateController.text = _fecha;
       });
     }
+  }
+
+  Widget _createDropDown() {
+    return Row(
+      children: <Widget>[
+        Icon( Icons.select_all ),
+        SizedBox( width: 30.0),
+        Expanded(
+            child: DropdownButton(
+            value: _optionSelected,
+            items: _getItemsDropDown(),
+            onChanged: ( option ) {
+              setState(() {
+                _optionSelected = option;
+              });
+            }
+          ),
+        ),
+      ],
+    );
+  }
+
+  List<DropdownMenuItem<String>> _getItemsDropDown() {
+    List<DropdownMenuItem<String>> list = new List();
+
+    _powers.forEach( ( power ) {
+      list.add(
+        DropdownMenuItem(
+          child: Text(power),
+          value: power,
+        )
+      );
+    });
+
+    return list;
+  }
+
+  Widget _createPerson() {
+    return ListTile(
+      title: Text('Nombre: $_name '),
+      subtitle: Text('Email: $_email '),
+      trailing: Text(_optionSelected),
+    );
   }
 
 }
